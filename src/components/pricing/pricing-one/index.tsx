5c889086c78119c4939c06f31cf7a4f4aa9d53e7
Neljohn-Cerera <neljohncerera.github@gmail.com>
2022-07-28 01:01:16 -0700

    pricing one done

--- src/components/pricing/pricing-one/index.tsx
+++ src/components/pricing/pricing-one/index.tsx
@@ -4,7 +4,7 @@ import Pricing from "./pricing";
 const PricingOne = () => {
   return (
     <div className="mb-12 p-4 bg-gray-50">
-      <h3 className="text-xl font-normal tracking-wide">DESIGN ONE</h3>
+      <h3 className="text-xl font-normal tracking-wide">STORAGE PRICING</h3>
       <div className="flex flex-col justify-center items-center ">
         <h4 className="text-base font-medium mb-8">Our Pricing</h4>
         <div className="flex mb-8">
@@ -28,9 +28,30 @@ const PricingOne = () => {
         </div>
 
         <div className="flex space-x-4 h-96">
-          <Pricing title="Basic" size="base" price="$100.00"/>
-          <Pricing title="Professional" size="large" price="$200.00"/>
-          <Pricing title="Enterprise" size="base" price="$300.00"/>
+          <Pricing
+            title="Basic"
+            size="base"
+            price="$100.00"
+            storage="100"
+            user="1"
+            send="1"
+          />
+          <Pricing
+            title="Professional"
+            size="large"
+            price="$200.00"
+            storage="200"
+            user="3"
+            send="2"
+          />
+          <Pricing
+            title="Enterprise"
+            size="base"
+            price="$300.00"
+            storage="300"
+            user="5"
+            send="3"
+          />
         </div>
       </div>
     </div>
