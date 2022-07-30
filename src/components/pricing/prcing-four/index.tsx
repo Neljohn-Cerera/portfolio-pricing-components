5d24e86e67bb92a610628d929829c2e3c5f255d7
Neljohn-Cerera <neljohncerera.github@gmail.com>
2022-07-29 23:45:04 -0700

    design four done

--- src/components/pricing/prcing-four/index.tsx
+++ src/components/pricing/prcing-four/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Pricing from "./pricing";
 
 const PricingFour = () => {
   return (
     <div className="mb-12 p-4 bg-gray-50">
       <h3 className="text-xl font-normal tracking-wide mb-4">DESIGN 4</h3>
       <div className="flex flex-col justify-center items-center">
-        <div className="flex space-x-0 justify-center items-center">
-          <p>Price four</p>
+        <div className="flex w-full justify-center items-center">
+          <Pricing feature />
+          <Pricing title="Premium" price="$78.00" />
+          <Pricing title="Plus" price="$57.00" plus />
+          <Pricing title="Basic" price="$19.95" basic />
         </div>
       </div>
     </div>
